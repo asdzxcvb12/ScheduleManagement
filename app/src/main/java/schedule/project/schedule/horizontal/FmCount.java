02a2e8a9c3feeb2f7f54fb0449526b2d9210465c
asdzxcvb12 <asdzxcvb12@naver.com>
2017-05-30 11:15:09 +0900

    test Log

--- app/src/main/java/schedule/project/schedule/horizontal/FmCount.java
+++ app/src/main/java/schedule/project/schedule/horizontal/FmCount.java
@@ -4,20 +4,19 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import schedule.project.schedule.R;
 
-/**
- * Created by ghangsub on 2017-04-24.
- */
-
 @SuppressLint("ValidFragment")
 public class FmCount extends Fragment {
-    View view;
-    Context mContext;
+
+    private View view;
+    private Context mContext;
+    private String TagMSG = "TagMSG";
 
     public FmCount(Context mContext) {
         this.mContext = mContext;
@@ -26,6 +25,8 @@ public class FmCount extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fm_count, container, false);
+
+        Log.d(TagMSG, "Message");
 
         return view;
     }
